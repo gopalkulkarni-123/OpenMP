f6fa49b09ec913d92eef90436ed2da7827b04968
Gopal Damodar Kulkarni <gopaldamodarkulkarni#gmail.com>
2025-03-18 11:57:56 +0100

    Change in the job file

--- Heat_Equation/experiment.sh
+++ Heat_Equation/experiment.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 # Loop through grid sizes from 1000 to 5000 in steps of 1000
-for n in 1000 2000 3000 4000 5000; do
+for n in 2 4 8 16 32; do
     # Compute cut points based on n
-    cut1=$((n / 4))
-    cut2=$((n / 2))
-    cut3=$((3 * n / 4))
-    
+    #cut1=$((n / 4))
+    #cut2=$((n / 2))
+    #cut3=$((3 * n / 4))
+
+    # Set the number of threads 
+    export OMP_NUM_THREADS=$n
+    export OMP_SCHEDULE="dynamic"
+    echo $OMP_NUM_THREADS
+	
     # Define the output file for this grid size
-    output_file="Results/experiment_${n}.gz"
-    time_log="Results/timeValues${n}.txt"
+    output_file="./Results/NumThrds_2_experiment_${n}.csv"
+    time_log="./Results/NumThrds_2_timeValues_${n}.txt"
     
     # Measure the start time
     start_time=$(date +%s%N)
     
     # Run the command and log time values
-    echo "Running heat equation for grid size ${n}x${n}..."
-    ./heatEqnTrial "$output_file" "$n" "$n" 3 3 "$cut1" "$cut2" "$cut3" "$cut1" "$cut2" "$cut3" > "$time_log"
+    echo "Running heat equation for grid size 200 x 200 for threads "$n" ..."
+    
+    #./heatEqnTrial "$output_file" "$n" "$n" 3 3 "$cut1" "$cut2" "$cut3" "$cut1" "$cut2" "$cut3" > "$time_log"
+    
+    ./heatEqnTrial "$output_file" "200" "200" 0 0 > "$time_log"
     
     # Measure the end time
     end_time=$(date +%s%N)
@@ -26,6 +34,9 @@ for n in 1000 2000 3000 4000 5000; do
     
     # Append elapsed time to log file
     echo "Execution time: ${elapsed_time} ms" >> "$time_log"
+    
+    # Sleep for 10 seconds
+    sleep 60
 done
 
 echo "All experiments completed."
